2ee423ee5be186d45c050c4c6fee01796c8ea3ff
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-09-23 12:10:42 -0300

    add estrutura projeto

---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import { error } from 'console'
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error("Invalid enviroment variables", _env.error.format())
+
+  throw new Error('Invalid enviroment variable')
+
+}
+
+
+export const env = _env.data;
