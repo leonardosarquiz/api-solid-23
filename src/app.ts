85953c89ad35f770920b008d004cf0d7b259fe6b
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-09-25 10:17:41 -0300

    add eslint and prisma migrations

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+/* eslint-disable prettier/prettier */
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    nome: 'Leonardo sarquiz',
+    email: 'leo@gmail.com',
+  },
+})
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import { app } from "./app";
-import { env } from "./env";
+/* eslint-disable prettier/prettier */
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT
-}).then(() => {
-  console.log('HTTP Sever Hunning!')
-})
+app
+  .listen({
+
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Sever Hunning!')
+  })
